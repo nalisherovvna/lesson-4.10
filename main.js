6e407f832536b79c9e881ffebdecd39bb4521f7f
nalisherovvna <mushknasiba@gmail.com>
2024-03-27 11:06:13 +0500

    it was my homework

---
+++ main.js
@@ -0,0 +1,30 @@
+// function myFuncsiya(); {
+//     console.log("Hello Mars");
+// }
+
+
+// function myFunction(love) {
+//     console.log("I love UZB");
+// }
+
+// myFunction();
+// myFunction();
+// myFunction();
+
+
+// let hisob = () => {
+//    console.log(Math.sqrta(10, 10));
+// }
+
+// hisob();
+
+
+function max(a,b) {
+    let c;
+    c = Math.max(a,b);
+    alert("kiritgan ikkta sonni kattasi = " + c);
+}
+
+let birinchi = Number(prompt("birinchi sonni kiriting..."));
+let ikkinchi = Number(prompt("ikkinchi sonni kiritng..."));
+max(birinchi,ikkinchi);
